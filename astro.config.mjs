b18ebe9b9af679063618937e21faa0c5af2c17fc
BiaAhmed <biaahmed12@gmail.com>
2025-05-13 20:39:19 +0500

    Test Static Build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,19 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
-    site: "https://www.rabiaahmed.me",
-    integrations: [mdx(), sitemap({
-        changefreq: "daily",
-        priority: 1,
-        lastmod: new Date().toISOString().split("T")[0],
-		}), tailwind(), react()],
-    output: "server",
-    adapter: cloudflare(),
+  site: "https://www.rabiaahmed.me",
+  output: "static",
+  integrations: [
+    mdx(),
+    sitemap({
+      changefreq: "daily",
+      priority: 1,
+      lastmod: new Date().toISOString().split("T")[0],
+    }),
+    tailwind(),
+    react(),
+  ],
 });
